e8f2bd63d22b017d0741c726fa319eacfe46fe4c
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2023-07-18 20:55:17 +0300

    uploaded ex1.c

---
+++ c_Programming/lecture_3_assignment/homework1/ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    printf("##########Console-output###\n");
+    printf("C Programming\n");
+    printf("###########################\n\n");
+    printf("########################################################################");
+    return 0;
+}
