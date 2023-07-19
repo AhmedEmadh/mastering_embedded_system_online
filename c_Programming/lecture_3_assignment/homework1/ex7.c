036e7635138e1655ec25a98ad56113f33d22ae4b
AhmedEmadh <104726251+AhmedEmadh@users.noreply.github.com>
2023-07-19 03:34:06 +0300

    added new features to ex7.c

--- c_Programming/lecture_3_assignment/homework1/ex7.c
+++ c_Programming/lecture_3_assignment/homework1/ex7.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+int main(void){
+	float a,b;
+	printf("Enter the value of number a: ");
+	fflush(stdout);
+	scanf("%f",&a);
+	printf("Enter the value of number b: ");
+	fflush(stdout);
+	scanf("%f",&b);
+	fflush(stdin);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("After swapping, value of a = %f\n",a);
+	printf("After swapping, value of b = %f\n",b);
+	return 0;
+}
